cdaa575c86b1ec67e1d1c758257809d2b4813db2
Jade-Rung-GH <jaitnipithe@hotmail.com>
2025-01-06 16:04:12 +0700

    finished top and mid content of the website

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        bg: "#1f2937",
+        placeholder: "#6d747d",
+        button: "#3c82f6",
+      },
+    },
+  },
+  plugins: [],
+};
